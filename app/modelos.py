54da932234c1d3dfe7f04b96719a60cc0e2da533
Kevin Heredia <kevin@ravecodesolutions.com>
2024-07-21 12:40:27 -0500

    refatctor: creacion de interfaz Mdeol y de DAO_CSV como clase concreta y con model como atributo de clase

--- app/modelos.py
+++ app/modelos.py
@@ -1,7 +1,14 @@
 from abc import ABC, abstractmethod
 import csv
 
-class Director:
+class Model(ABC):
+    
+    @classmethod
+    @abstractmethod
+    def create_from_dict(cls, diccionario):
+        pass
+
+class Director(Model):
     @classmethod
     def create_from_dict(cls, diccionario):
         return cls(diccionario["nombre"], int(diccionario["id"]))
@@ -23,7 +30,17 @@ class Director:
         return hash((self.id, self.nombre))
 
 
-class Pelicula:
+class Pelicula(Model):
+
+    @classmethod
+    def create_from_dict(cls, diccionario):
+        return cls(diccionario["titulo"], 
+                                      diccionario["sinopsis"], 
+                                      int(diccionario["director_id"]), 
+                                      int(diccionario["id"]))
+
+
+
     def __init__(self, titulo: str, sinopsis: str, director: object, id = -1):
         self.titulo = titulo
         self.sinopsis = sinopsis
@@ -81,31 +98,28 @@ class DAO(ABC):
         pass
 
 class DAO_CSV(DAO):
-    def __init__(self, path):
+
+    model = None
+
+    def __init__(self, path, encoding="utf-8"):
+        
         self.path = path
+        self.encoding = encoding
+    def todos(self):
+        with open(self.path, "r", newline="", encoding=self.encoding) as fichero:
+            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+            lista = []
+            for registro in lector_csv:
+                lista.append(self.model.create_from_dict(registro))
+        return lista   
 
 class DAO_CSV_Director(DAO_CSV):
     
 
-    def todos(self):
-        with open(self.path, "r", newline="") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Director.create_from_dict(registro))
-        return lista        
+    model = Director
+             
 
 class DAO_CSV_Pelicula(DAO_CSV):
    
 
-    def todos(self):
-        with open(self.path, "r", newline="", encoding="utf-8") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Pelicula(registro["titulo"], 
-                                      registro["sinopsis"], 
-                                      int(registro["director_id"]), 
-                                      int(registro["id"])))
-                
-        return lista
+    model = Pelicula
